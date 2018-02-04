1031cd5415c0913066bbadc4bb5c3ef396c43937
Dennis Walter <dennis.walter@gmail.com>
2018-02-04 14:35:34 +0100

    Host changed

--- mqttperf.py
+++ mqttperf.py
@@ -7,7 +7,7 @@ from socket import gethostname
 import time
 
 credentials = pika.PlainCredentials('perf', 'ferp777')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.11', 5672, '/', credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='mqtt', exchange_type='topic')
@@ -18,8 +18,8 @@ clob = b64encode(urandom(512)).decode('ascii')
 while True:
     msg = json.dumps({'id': str(uuid4()), 'seq': cnt, 'sender': sender_id, 'content': clob})
     channel.basic_publish(exchange='mqtt', routing_key='perf', body=msg)
-    print("Sent message number %d" % cnt)
+    # print("Sent message number %d" % cnt)
     cnt += 1
-    time.sleep(1)
+    #time.sleep(1)
     
 
